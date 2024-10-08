55a4c86dcbfbee89b629b1d4ed01a8c329b23ba9
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-10-08 15:56:44 +0200

    add creado por

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: '<router-outlet></router-outlet>',
-  styleUrl: './app.component.css'
+  template: `
+    <router-outlet></router-outlet>
+    <p class="text-futuristic-silver text-center text-sm m-8">Creado por <a class="text-rick-blue hover:text-rick-blue/80" href="https://github.com/ezequielgdl" target="_blank">Ezequiel Gómez de Lima</a></p>
+  `
 })
 export class AppComponent {
   title = 'rick-and-morty';
